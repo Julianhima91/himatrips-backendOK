7bc7c5c942b886e99fb362651434c483e1359866
Ireg27 <geri.manjani@gmail.com>
2024-11-12 13:13:42 +0100

    People hotel room divide

--- app/Http/Requests/Livesearch/LivesearchRequest.php
+++ app/Http/Requests/Livesearch/LivesearchRequest.php
@@ -13,9 +13,9 @@ class LivesearchRequest extends FormRequest
             'date' => ['required', 'date', 'date_format:Y-m-d'],
             'origin_id' => ['required', 'integer', 'exists:origins,id'],
             'destination_id' => ['required', 'integer', 'exists:destinations,id'],
-            'adults' => ['required', 'integer', 'min:1'],
-            'children' => ['required', 'integer'],
-            'infants' => ['required', 'integer'],
+            'adults' => ['required', 'integer', 'min:1', 'max:5'],
+            'children' => ['required', 'integer', 'max:5'],
+            'infants' => ['required', 'integer', 'max:5'],
             'rooms' => ['required', 'integer'],
         ];
     }
