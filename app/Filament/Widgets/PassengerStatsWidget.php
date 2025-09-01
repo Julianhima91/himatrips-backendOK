bb13c288f3cd07de8ee94fb5c6aa20b8df0fb85c
Ireg27 <geri.manjani@gmail.com>
2025-09-01 12:02:18 +0200

    Added a new widget to show all passenger details in dashboard

---
+++ app/Filament/Widgets/PassengerStatsWidget.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\FlightData;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget;
+
+class PassengerStatsWidget extends TableWidget
+{
+    protected static ?string $heading = 'Flights by Adults & Children';
+
+    protected int|string|array $columnSpan = 'full';
+
+    public function table(Table $table): Table
+    {
+        $query = FlightData::query()
+            ->select('adults', 'children')
+            ->selectRaw('COUNT(*) as total_flights')
+            ->selectRaw('CONCAT(adults, "-", children) as id')
+            ->groupBy('adults', 'children')
+            ->orderByDesc('total_flights');
+
+        return $table
+            ->query($query)
+            ->columns([
+                TextColumn::make('adults')
+                    ->label('Adults')
+                    ->sortable(),
+                TextColumn::make('children')
+                    ->label('Children')
+                    ->sortable(),
+                TextColumn::make('total_flights')
+                    ->label('Total Itineraries')
+                    //todo: remove dividing by 2 once enough time has passed for the return column to be correct in db
+                    ->formatStateUsing(fn ($record) => intval($record->total_flights / 2))
+                    ->sortable(),
+            ])
+            ->filters([
+                Tables\Filters\Filter::make('Specific Adults & Children')
+                    ->form([
+                        TextInput::make('adults'),
+                        TextInput::make('children'),
+                    ])
+                    ->query(function ($query, array $data) {
+                        $query->when($data['adults'], fn ($query, $value) => $query->where('adults', intval($value)));
+                        $query->when(isset($data['children']), fn ($query) => $query->where('children', intval($data['children'])));
+                    }),
+            ])
+            ->defaultSort('total_flights', 'desc');
+    }
+}
