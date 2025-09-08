aca04ecd478e578f0bccac2b9826921602d5354d
Ireg27 <geri.manjani@gmail.com>
2025-09-08 15:28:33 +0200

    Added date range filter

--- app/Filament/Widgets/PopularPackages.php
+++ app/Filament/Widgets/PopularPackages.php
@@ -100,54 +100,24 @@ class PopularPackages extends BaseWidget
 
                         return $query;
                     }),
+                Filter::make('date_range')
+                    ->label('Recorded At')
+                    ->schema([
+                        DatePicker::make('start_date')
+                            ->hint('Filtering may take a few seconds.')
+                            ->label('Start Date'),
+                        DatePicker::make('end_date')
+                            ->hint('Filtering may take a few seconds.')
+                            ->label('End Date'),
+                    ])
+                    ->query(function ($query, array $data) {
+                        if (! empty($data['start_date'])) {
+                            $endDate = $data['end_date'] ?? now()->toDateString();
+                            $query->whereBetween('psc.created_at', [$data['start_date'], $endDate]);
+                        }
 
-                //                Filter::make('date_range')
-                //                    ->label('Created At')
-                //                    ->schema([
-                //                        DatePicker::make('start_date')->label('Start Date'),
-                //                        DatePicker::make('end_date')->label('End Date'),
-                //                    ])
-                //                    ->query(function ($query, array $data) {
-                //                        $startDate = $data['start_date'] ?? now()->subMonths(3)->toDateString();
-                //                        $endDate = $data['end_date'] ?? now();
-                //
-                //                        return $query->whereBetween(DB::raw('DATE(latest_package_created_at)'), [$startDate, $endDate]);
-                //                    }),
-                //                Filter::make('origin_country')
-                //                    ->label('Origin Country')
-                //                    ->schema([
-                //                        Select::make('origin_country')
-                //                            ->options(fn () => DB::table('countries')->pluck('name', 'id'))
-                //                            ->searchable(),
-                //                    ])
-                //                    ->query(function ($query, array $data) {
-                //                        if (! empty($data['origin_country'])) {
-                //                            return $query->where('countries.id', $data['origin_country']);
-                //                        }
-                //
-                //                        return $query;
-                //                    }),
-                //
-                //                Filter::make('origin')
-                //                    ->label('Origin')
-                //                    ->schema([
-                //                        Select::make('origin_id')
-                //                            ->label('Select Origin')
-                //                            ->options(fn () => Cache::remember('origins-list', 3600, fn () => Origin::query()->pluck('name', 'id')
-                //                            ))
-                //                            ->reactive()
-                //                            ->searchable(),
-                //                    ])
-                //                    ->query(function ($query, array $data) {
-                //                        if (! empty($data['origin_id'])) {
-                //                            $this->originId = $data['origin_id'];
-                //
-                //                            return $query->where('destination_origins.origin_id', $data['origin_id']);
-                //                        }
-                //
-                //                        return $query;
-                //                    }),
-                //
+                        return $query;
+                    }),
                 //                Filter::make('airport')
                 //                    ->label('Origin Airport')
                 //                    ->schema([
@@ -166,65 +136,6 @@ class PopularPackages extends BaseWidget
                 //                    ->query(function ($query, array $data) {
                 //                        if (! empty($data['airport_id'])) {
                 //                            return $query->where('flight_data.origin', $data['airport_id']);
-                //                        }
-                //
-                //                        return $query;
-                //                    }),
-                //
-                //                Filter::make('destination_country')
-                //                    ->label('Destination Country')
-                //                    ->schema([
-                //                        Select::make('destination_country')
-                //                            ->options(fn () => DB::table('countries')->pluck('name', 'id'))
-                //                            ->searchable(),
-                //                    ])
-                //                    ->query(function ($query, array $data) {
-                //                        if (! empty($data['destination_country'])) {
-                //                            return $query->where('countries.id', $data['destination_country']);
-                //                        }
-                //
-                //                        return $query;
-                //                    }),
-                //
-                //                Filter::make('destination')
-                //                    ->label('Destination')
-                //                    ->schema([
-                //                        Select::make('destination_id')
-                //                            ->label('Select Destination')
-                //                            ->options(fn () => Cache::remember('destinations-list', 3600, fn () => Destination::query()->pluck('name', 'id')
-                //                            ))
-                //                            ->reactive()
-                //                            ->searchable(),
-                //                    ])
-                //                    ->query(function ($query, array $data) {
-                //                        if (! empty($data['destination_id'])) {
-                //                            $this->destinationId = $data['destination_id'];
-                //
-                //                            return $query->where('destination_origins.destination_id', $data['destination_id']);
-                //                        }
-                //
-                //                        return $query;
-                //                    }),
-                //                Filter::make('destination_airport')
-                //                    ->label('Destination Airport')
-                //                    ->schema([
-                //                        Select::make('destination_airport_id')
-                //                            ->label('Select Destination Airport')
-                //                            ->options(function () {
-                //                                if (! empty($this->destinationId)) {
-                //                                    return Airport::whereHas('destinations', function ($query) {
-                //                                        return $query->where('destination_id', $this->destinationId);
-                //                                    })->pluck('nameAirport', 'codeIataAirport');
-                //                                }
-                //
-                //                                return [];
-                //                            })
-                //                            ->searchable()
-                //                            ->reactive(),
-                //                    ])
-                //                    ->query(function ($query, array $data) {
-                //                        if (! empty($data['destination_airport_id'])) {
-                //                            return $query->where('flight_data.destination', $data['destination_airport_id']);
                 //                        }
                 //
                 //                        return $query;
